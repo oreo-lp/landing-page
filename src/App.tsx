1000fb28a4801888bd8f9c0c94ef488c2b821fe2
penglu <15161172959@163.com>
2025-11-23 00:59:06 +0800

    [feat] 新增Services组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Layout } from "./components/Layout";
 import { Brands } from "./components/sections/Brands";
 import { Hero } from "./components/sections/Hero";
+import { Services } from "./components/sections/Services";
+
 function App() {
   return (
     <>
       <Layout title="EageAI">
         <Hero />
         <Brands />
+        <Services />
       </Layout>
     </>
   );
