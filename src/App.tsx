97ce1bc50fbe58c035978bebb1668f8a4fc9756a
penglu <15161172959@163.com>
2025-11-23 20:45:18 +0800

    [feat] 新增价格部分

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Layout } from "./components/Layout";
 import { AboutUs } from "./components/sections/AboutUs";
 import { Brands } from "./components/sections/Brands";
 import { Hero } from "./components/sections/Hero";
+import { Pricing } from "./components/sections/Pricing";
 import { Services } from "./components/sections/Services";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Brands />
         <Services />
         <AboutUs />
+        <Pricing />
       </Layout>
     </>
   );
