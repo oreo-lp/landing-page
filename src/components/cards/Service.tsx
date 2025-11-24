df0a4c4b218b95c04d37a7db0d14590218b9dd19
penglu <15161172959@163.com>
2025-11-24 20:42:44 +0800

    [feat] 新增footer部分

--- src/components/cards/Service.tsx
+++ src/components/cards/Service.tsx
@@ -12,7 +12,7 @@ export const Service = ({ title, description, icon }: ServiceProps) => {
       className="p-5 md:p-6 lg:p-8 rounded-3xl border border-box-border bg-box-bg shadow-lg
                     shadow-box-shadow relative overflow-hidden"
     >
-      <div className="rounded-xl bg-gray-300 dark:bg-gray-950 p-3 text-heading-1 w-max relative">
+      <div className="rounded-xl bg-body dark:bg-gray-950 p-3 text-heading-1 w-max relative">
         {" "}
         {icon}{" "}
       </div>
